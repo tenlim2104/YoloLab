8e5c75a7bde78c9017e68badb2c696fed65c778b
Tong En Lim <128690808+tenlim2104@users.noreply.github.com>
2025-07-15 16:11:35 +0800

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,30 @@
+sudo apt install python3-picamera2 -y
+
+# Additional picamera2 dependencies
+sudo apt install python3-libcamera -y
+sudo apt install python3-kms++ -y
+sudo apt install python3-prctl -y
+sudo apt install libcap-dev -y
+
+#sudo apt install python3-opencv -y
+
+# Option 2: Install via pip (if you need latest version)
+pip install opencv-python
+pip install opencv-contrib-python
+
+sudo apt install libcamera-apps -y
+sudo apt install libcamera-dev -y
+sudo apt install libcamera-tools -y
+
+# Image processing libraries
+sudo apt install libjpeg-dev -y
+sudo apt install libpng-dev -y
+sudo apt install libtiff-dev -y
+sudo apt install libavcodec-dev -y
+sudo apt install libavformat-dev -y
+sudo apt install libswscale-dev -y
+sudo apt install libv4l-dev -y
+
+sudo apt install python3-numpy -y
+
+pip install ultralytics
